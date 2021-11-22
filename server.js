0f68882cf2d020b6f67f39456d144618acaf2505
willchrist <william.christian@rga.com>
2021-11-22 13:26:55 +0700

    [will] initial commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const fetch = require('node-fetch');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+// app.use('/api', router);
+app.get('/', (req, res) => {
+    res.status(200).json({
+        success: true,
+        message: "Welcome to To Do API!"
+    });
+});
+
+app.post('/', (req, res) => {
+    const body = {'content': 'message content'};
+
+    const response = fetch('https://discord.com/api/webhooks/912190240448466956/6c00ssVfVdQPZWBpgOnjeHEeH22C62DkcUTf-iCFtfImD7aEzhgkMcH5Tw_DGc1MMT5N', {
+        method: 'post',
+        body: JSON.stringify(body),
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    res.status(200).json(body);
+});
+
+app.listen(port, () => {
+    console.log(`Server started at ${Date()}!`);
+    console.log(`Listening on port ${port}!`);
+})
+
+module.exports = app;
